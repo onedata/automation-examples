4d5784cd99710a91c8c940feecfc54b6fd58d2f8
duleba.pio <duleba.pio@gmail.com>
2021-07-13 10:22:42 +0200

    VFS-7844 cleanup repo

---
+++ fetch-data/function/handler.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+
+def handle(req: bytes):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+    args = json.loads(req)
+
+    file_path = f'/mnt/onedata/.__onedata_file_id_{args["fetch_file"]["file_id"]}'
+    target_dir_id = args["target_dir"]["file_id"]
+
+    with open(file_path, "rb") as fd:
+        line = fd.readline()
+        while line:
+            parts = line.decode("utf-8").strip().split(" ")
+            url = parts[0]
+            file_name = parts[2]
+            r = requests.get(url, stream=True, allow_redirects=True)
+            target_file_path = f'/mnt/onedata/.__onedata__file_id__{target_dir_id}/{file_name}'
+            with open(target_file_path, 'wb') as f:
+                for chunk in r.iter_content(32 * 1024):
+                    f.write(chunk)
+            line = fd.readline()
+
+    return json.dumps({})
--- file-format-detection/function/handler.py
+++ file-format-detection/function/handler.py
@@ -5,6 +5,7 @@ from guesslang import Guess
 
 BLOCK_SIZE = 262144
 
+
 def handle(req: bytes):
     """handle a request to the function
     Args:
@@ -13,13 +14,9 @@ def handle(req: bytes):
     args = json.loads(req)
 
     file_id = args["item"]["file_id"]
-    file_name = args["item"]["name"]
     file_path = f'/mnt/onedata/.__onedata__file_id__{file_id}'
     metadata_key = args["metadata_key"]
     file_format = get_file_format(file_path)
-
-    inferred_file_type = infer_file_type(file_name, file_path)
-
     if metadata_key != "":
         xd = xattr.xattr(file_path)
         xd.set(metadata_key, str.encode(file_format))
@@ -31,41 +28,3 @@ def get_file_format(file_path):
     file_format = file_info.split(",")[0]
     file_format_underscore = file_format.replace(" ", "_")
     return file_format_underscore
-
-
-def infer_file_type(file_name, file_path):
-    file_extension_category = get_file_extension_category(file_name)
-    if file_extension_category == "code":
-        return "Python"
-    elif file_extension_category == "data":
-        return "Csv"
-    else
-        return
-
-
-def get_file_extension_category(file_name):
-    return "code"
-
-# def handle(req: bytes):
-#     """handle a request to the function
-#     Args:
-#         req (str): request body
-#     """
-#     args = json.loads(req)
-#
-#     file_id = args["item"]["file_id"]
-#     file_path = f'/mnt/onedata/.__onedata__file_id__{file_id}'
-#     metadata_key = args["metadata_key"]
-#     file_format = get_file_format(file_path)
-#     if metadata_key != "":
-#         xd = xattr.xattr(file_path)
-#         xd.set(metadata_key, str.encode(file_format))
-#     return json.dumps({"format": file_format})
-#
-#
-# def get_file_format(file_path):
-#     file_info = magic.from_file(file_path)
-#     file_format = file_info.split(",")[0]
-#     file_format_underscore = file_format.replace(" ", "_")
-#     return file_format_underscore
-
