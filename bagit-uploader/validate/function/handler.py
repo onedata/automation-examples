bfd12b73f5334926446efc3c6db733cec457ae1f
duleba.pio <duleba.pio@gmail.com>
2021-07-21 10:49:18 +0200

    VFS-7844 resolve PR tasks in validate lambda

--- bagit-uploader/validate/function/handler.py
+++ bagit-uploader/validate/function/handler.py
@@ -1,20 +1,22 @@
+from collections.abc import Callable
 import hashlib
 import json
 import os.path
-import tarfile
-import zipfile
 import re
-import zlib
+import tarfile
+from typing import IO
 import time
+import zipfile
+import zlib
+
 import requests
 
-SUPPORTED_CHECKSUM_ALGORITHMS = ('md5', 'sha1', 'sha256', 'sha512', 'adler32')
+SUPPORTED_CHECKSUM_ALGORITHMS: tuple = ('md5', 'sha1', 'sha256', 'sha512', 'adler32')
+BLOCK_SIZE: int = 262144
 
-BLOCK_SIZE = 262144
-
-HEARTBEAT_CYCLE = 150
-LAST_HEARTBEAT_TIME = 0
-HEARTBEAT_URL = ""
+HEARTBEAT_INTERVAL_SEC: int = 150
+LAST_HEARTBEAT_TIME: int = 0
+HEARTBEAT_URL: str = ""
 
 
 def handle(req: bytes):
@@ -49,94 +51,54 @@ def handle(req: bytes):
     return json.dumps({"validBagitArchives": valid_bagit_archives})
 
 
-def assert_valid_bagit_archive(archive_path, archive_filename):
+def assert_valid_bagit_archive(archive_path: str, archive_filename: str):
     archive_name, archive_type = os.path.splitext(archive_filename)
 
     if archive_type == '.tar':
-        assert_valid_tar_bagit_archive(archive_path)
+        with tarfile.open(archive_path) as archive:
+            assert_valid_archive(archive.getnames, archive.extractfile)
     elif archive_type == '.zip':
-        assert_valid_zip_bagit_archive(archive_path)
+        with zipfile.ZipFile(archive_path) as archive:
+            assert_valid_archive(archive.namelist, archive.open)
     elif archive_type == '.tgz' or archive_type == ".gz":
-        assert_valid_tgz_bagit_archive(archive_path)
+        with tarfile.open(archive_path, "r:gz") as archive:
+            assert_valid_archive(archive.getnames, archive.extractfile)
 
 
-def assert_valid_tar_bagit_archive(archive_path):
-    with tarfile.TarFile(archive_path) as archive:
-        archive_files = archive.getnames()
+def assert_valid_archive(
+        list_archive_files: Callable[[], list],
+        open_archive_file: Callable[[str], IO[bytes]]):
+    archive_files = list_archive_files()
+    bagit_dir = find_bagit_dir(archive_files)
+    assert bagit_dir is not None
 
-        bagit_dir = find_bagit_dir(archive_files)
-        assert bagit_dir is not None
+    with open_archive_file(f'{bagit_dir}/bagit.txt') as f:
+        assert_proper_bagit_txt_content(f)
 
-        data_dir = f'{bagit_dir}/data'
-        fetch_file = f'{bagit_dir}/fetch.txt'
-        assert (data_dir in archive_files) or (fetch_file in archive_files)
+    data_dir = f'{bagit_dir}/data'
+    fetch_file = f'{bagit_dir}/fetch.txt'
+    assert (data_dir in archive_files) or (fetch_file in archive_files)
 
-        for algorithm in SUPPORTED_CHECKSUM_ALGORITHMS:
-            tagmanifest_file = f'{bagit_dir}/tagmanifest-{algorithm}.txt'
+    for algorithm in SUPPORTED_CHECKSUM_ALGORITHMS:
+        tagmanifest_file = f'{bagit_dir}/tagmanifest-{algorithm}.txt'
 
-            if tagmanifest_file not in archive_files:
-                continue
+        if tagmanifest_file not in archive_files:
+            continue
 
-            for line in archive.extractfile(tagmanifest_file):
-                exp_checksum, file_rel_path = line.decode('utf-8').strip().split()
-                fd = archive.extractfile(f'{bagit_dir}/{file_rel_path}')
-                assert exp_checksum == calculate_checksum(fd, algorithm)
-
-
-def assert_valid_zip_bagit_archive(archive_path):
-    with zipfile.ZipFile(archive_path) as archive:
-        archive_files = archive.namelist()
-
-        bagit_dir = find_bagit_dir(archive_files)
-        assert bagit_dir is not None
-
-        data_dir = f'{bagit_dir}/data'
-        fetch_file = f'{bagit_dir}/fetch.txt'
-        assert (data_dir in archive_files) or (fetch_file in archive_files)
-
-        for algorithm in SUPPORTED_CHECKSUM_ALGORITHMS:
-            tagmanifest_file = f'{bagit_dir}/tagmanifest-{algorithm}.txt'
-
-            if tagmanifest_file not in archive_files:
-                continue
-            with archive.open(tagmanifest_file) as tmf:
-                for line in tmf:
-                    exp_checksum, file_rel_path = line.decode('utf-8').strip().split()
-                    fd = archive.open(f'{bagit_dir}/{file_rel_path}')
-                    assert exp_checksum == calculate_checksum(fd, algorithm)
+        for line in open_archive_file(tagmanifest_file):
+            exp_checksum, file_rel_path = line.decode('utf-8').strip().split()
+            fd = open_archive_file(f'{bagit_dir}/{file_rel_path}')
+            assert exp_checksum == calculate_checksum(fd, algorithm)
 
 
-def assert_valid_tgz_bagit_archive(archive_path):
-    with tarfile.open(archive_path, "r:gz") as archive:
-        archive_files = archive.getnames()
-
-        bagit_dir = find_bagit_dir(archive_files)
-        assert bagit_dir is not None
-
-        data_dir = f'{bagit_dir}/data'
-        fetch_file = f'{bagit_dir}/fetch.txt'
-        assert (data_dir in archive_files) or (fetch_file in archive_files)
-
-        for algorithm in SUPPORTED_CHECKSUM_ALGORITHMS:
-            tagmanifest_file = f'{bagit_dir}/tagmanifest-{algorithm}.txt'
-
-            if tagmanifest_file not in archive_files:
-                continue
-
-            for line in archive.extractfile(tagmanifest_file):
-                exp_checksum, file_rel_path = line.decode('utf-8').strip().split()
-                fd = archive.extractfile(f'{bagit_dir}/{file_rel_path}')
-                assert exp_checksum == calculate_checksum(fd, algorithm)
-
-
-def find_bagit_dir(archive_files):
+def find_bagit_dir(archive_files: list):
     for file_path in archive_files:
         dir_path, file_name = os.path.split(file_path)
         if file_name == 'bagit.txt':
             return dir_path
 
 
-def calculate_checksum(fd, algorithm):
+def calculate_checksum(fd, algorithm: str):
     if algorithm == "adler32":
         checksum = 1
         while True:
@@ -159,15 +121,20 @@ def calculate_checksum(fd, algorithm):
 
 def assert_proper_bagit_txt_content(fd):
     content = fd.readlines()
-    for index, line in enumerate(content):
-        key = line.decode("utf-8").trim().split(":")[0]
-        if key == "BagIt-Version":
-            bagit_version = line.decode("utf-8").trim().split(":")[1]
-            assert_correct_bagit_version(bagit_version)
-            next_line_key = content[index + 1].decode("utf-8").trim().split(":")[0]
-            next_line_value = content[index + 1].decode("utf-8").trim().split(":")[1]
-            assert next_line_key == "Tag-File-Character-Encoding"
-            assert next_line_value != ""
+
+    assert len(content) == 2
+
+    key1 = content[0].decode("utf-8").split(":")[0].strip()
+    assert key1 == "BagIt-Version"
+
+    bagit_version = content[0].decode("utf-8").split(":")[1].strip()
+    assert_correct_bagit_version(bagit_version)
+
+    key2 = content[1].decode("utf-8").split(":")[0].strip()
+    assert key2 == "Tag-File-Character-Encoding"
+
+    encoding = content[1].decode("utf-8").split(":")[1].strip()
+    assert encoding != ""
 
 
 def assert_correct_bagit_version(bagit_version):
@@ -178,7 +145,7 @@ def assert_correct_bagit_version(bagit_version):
 def heartbeat():
     global LAST_HEARTBEAT_TIME
     current_time = int(time.time())
-    if current_time - LAST_HEARTBEAT_TIME > 150:
+    if current_time - LAST_HEARTBEAT_TIME > HEARTBEAT_INTERVAL_SEC:
         r = requests.post(url=HEARTBEAT_URL, data={})
-        assert r.ok
-        LAST_HEARTBEAT_TIME = current_time
+        if r.ok:
+            LAST_HEARTBEAT_TIME = current_time
