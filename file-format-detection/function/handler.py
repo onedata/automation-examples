4577180fa3ee08ccb81559380d97bf7410f05c1e
duleba.pio <duleba.pio@gmail.com>
2021-08-05 09:07:03 +0200

    VFS-7842 add description

--- file-format-detection/function/handler.py
+++ file-format-detection/function/handler.py
@@ -9,7 +9,7 @@ import magic
 from guesslang import Guess
 import pandas as pd
 
-validation_mapping = {
+validation_mapping: dict = {
     '.py': {
         'content': 'Python',
         'validator': "verify_language"
@@ -25,10 +25,11 @@ validation_mapping = {
 }
 
 
-def handle(req: bytes):
-    """handle a request to the function
+def handle(req: bytes) -> str:
+    """Function returns file formats, based on its name and content. Sets results as metadata.
     Args:
-        req (str): request body
+        item (Any-File): file to process
+        metadata_key (String): namespace key of metadata entry
     """
     args = json.loads(req)
 
@@ -37,12 +38,13 @@ def handle(req: bytes):
     metadata_key = args["metadata_key"]
 
     if args["item"]["type"] == "REG":
-        file_type = get_mime_content_type(file_path),
+        file_type = get_mime_filename_type(args["item"]["name"]),
+        file_type_str = str(file_type)
         file_name, file_extension = os.path.splitext(args["item"]["name"])
 
         if metadata_key != "":
             xd = xattr.xattr(file_path)
-            xd.set(f"{metadata_key}.mime-type", str.encode(file_type[0]))
+            xd.set(f"{metadata_key}.format-extension", str.encode(str(file_type_str)))
 
         if file_extension in validation_mapping:
             validator_name = validation_mapping[file_extension]["validator"]
@@ -51,14 +53,14 @@ def handle(req: bytes):
 
             if metadata_key != "":
                 xd = xattr.xattr(file_path)
-                xd.set(f"{metadata_key}.content-match", str.encode(str(content_match)))
-                xd.set(f"{metadata_key}.inferred-content", str.encode(inferred_content))
+                xd.set(f"{metadata_key}.is-extension-matching-content", str.encode(str(content_match)))
+                xd.set(f"{metadata_key}.format-content", str.encode(inferred_content))
 
             return json.dumps({
                 "format": {
                     "file": args["item"]["name"],
-                    "fileType": file_type,
-                    "contentMatch": content_match,
+                    "format-extension": file_type,
+                    "is-extension-matching-content": content_match,
                     "inferredContent": inferred_content
                 }})
 
@@ -66,22 +68,22 @@ def handle(req: bytes):
             return json.dumps({
                 "format": {
                     "file": args["item"]["name"],
-                    "fileType": file_type,
-                    "contentMatch": "unsupported file extension for content inferring"
+                    "format-extension": file_type,
+                    "is-extension-matching-content": "unsupported file extension for content inferring"
                 }})
     return json.dumps({
         "format": {
             "file": args["item"]["name"],
-            "fileType": "not a regular file",
+            "format-extension": "not a regular file",
         }})
 
 
-def extension_match_content(file_path, file_extension):
+def extension_match_content(file_path: str, file_extension: str) -> str:
     validator_name = validation_mapping[file_extension]["validator"]
     return globals()[validator_name](file_extension, file_path)
 
 
-def infer_content(file_path):
+def infer_content(file_path: str) -> str:
     possible_content_types = []
     for extension in validation_mapping:
         validator_name = validation_mapping[extension]["validator"]
@@ -96,16 +98,16 @@ def infer_content(file_path):
         return "unable to infer content type"
 
 
-def get_mime_filename_type(file_path):
-    type, encoding = mimetypes.guess_type(file_path, strict=True)
-    return type
+def get_mime_filename_type(file_path: str) -> str:
+    result = mimetypes.guess_type(file_path, strict=True)
+    return "".join(result[0])
 
 
-def get_mime_content_type(file_path):
+def get_mime_content_type(file_path: str) -> str:
     return magic.from_file(file_path, mime=True)
 
 
-def guess_content_code_language(file_path):
+def guess_content_code_language(file_path: str) -> str:
     guess = Guess()
     with open(file_path, 'rb') as file:
         data = file.read()
@@ -113,7 +115,7 @@ def guess_content_code_language(file_path):
     return language
 
 
-def verify_language(expected_language, file_path):
+def verify_language(expected_language: str, file_path: str) -> bool:
     guess = Guess()
     with open(file_path, 'rb') as file:
         data = file.read()
@@ -121,10 +123,9 @@ def verify_language(expected_language, file_path):
     return bool(language == expected_language)
 
 
-def validate_csv(expected_format, file_path):
+def validate_csv(expected_format: str, file_path: str) -> bool:
     try:
         pd.read_csv(file_path)
         return True
     except:
         return False
-
