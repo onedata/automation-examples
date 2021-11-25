ec5ab68ef57cb90125c57d1d2b5f45ab80e61388
duleba.pio <duleba.pio@gmail.com>
2021-11-25 14:05:38 +0100

    VFS-8664 update code for hello-world-file lambda, add makefile

--- hello-world-file/function/handler.py
+++ hello-world-file/function/handler.py
@@ -2,12 +2,16 @@ import json
 
 
 def handle(req: bytes):
-    """handle a request to the function
+    """Takes files and returns its name with "Hello" prefix.
     Args:
         req (str): request body
     """
     args = json.loads(req)
+    results = [process_item(file) for file in args["resultsBatch"]]
+    return json.dumps({"resultsBatch": results})
 
-    file_name = args["item"]["name"]
 
-    return json.dumps({"response": f'Hello - {file_name}'})
+def process_item(file):
+    file_name = file["item"]["name"]
+    return {"response": f'Hello - {file_name}'}
+
