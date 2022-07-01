01604bf32defe3e40714a641cc2661f86f98b1b1
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-07-01 15:06:11 +0200

    VFS-9542 PR fixes

--- file-format-detection/function/handler.py
+++ file-format-detection/function/handler.py
@@ -5,11 +5,11 @@ import xattr
 import magic
 from typing import NamedTuple
 
+
 class FileFormat(NamedTuple):
     name: str
     mime_type: str
     extensions: list[str]
-    is_extension_matching_format: bool
 
 
 def handle(req: bytes) -> str:
@@ -20,7 +20,7 @@ def handle(req: bytes) -> str:
     """
     data = json.loads(req)
 
-    results = [process_item(item) for item in data['argsBatch']]
+    results = [process_item(item) for item in data["argsBatch"]]
 
     return json.dumps({"resultsBatch": results})
 
@@ -33,34 +33,37 @@ def process_item(args):
         return create_failure_result(file, "NOT_A_REGULAR_FILE")
 
     try:
-        file_format = get_reg_file_format(file)
+        file_format = get_file_format(file)
+        is_extension_matching_format = check_file_extension_matches_format(
+            file["name"], file_format
+        )
 
         if metadata_key != "":
-            set_file_format_xattrs(file, metadata_key, file_format)
+            set_file_format_xattrs(
+                file, metadata_key, file_format, is_extension_matching_format
+            )
 
-        return create_success_result(file, file_format)
+        return create_success_result(file, file_format, is_extension_matching_format)
 
     except Exception as ex:
         return create_failure_result(file, str(ex))
 
 
 def create_empty_result(file: dict) -> dict:
-    return {
-        "result": {
-            "file-id": file["file_id"],
-            "file-name": file["name"]
-        }
-    }
+    return {"result": {"file-id": file["file_id"], "file-name": file["name"]}}
 
-def create_success_result(file: dict, file_format: FileFormat) -> dict:
+
+def create_success_result(
+    file: dict, file_format: FileFormat, is_extension_matching_format: bool
+) -> dict:
     result = create_empty_result(file)
     result["result"]["detection-passed"] = True
     result["result"]["format"] = {
         "name": file_format.name,
         "mime-type": file_format.mime_type,
         "extensions": file_format.extensions,
-        "is-extension-matching-format": file_format.is_extension_matching_format
     }
+    result["result"]["is-extension-matching-format"] = is_extension_matching_format
     return result
 
 
@@ -75,23 +78,25 @@ def get_file_path(file: dict) -> str:
     return f"/mnt/onedata/.__onedata__file_id__{file['file_id']}"
 
 
-def get_reg_file_format(reg_file: dict) -> FileFormat:
-    file_path = get_file_path(reg_file)
+def get_file_format(file: dict) -> FileFormat:
+    file_path = get_file_path(file)
 
     format_name = get_file_format_name(file_path)
     mime_type = get_file_mime_type(file_path)
-
     format_extensions = get_mime_type_extensions(mime_type)
-    _, used_extension = os.path.splitext(reg_file["name"])
-    is_extension_matching_format = len(format_extensions) == 0 or \
-                                   used_extension in format_extensions
 
     return FileFormat(
-        name = format_name,
-        mime_type = mime_type,
-        extensions = format_extensions,
-        is_extension_matching_format = is_extension_matching_format
+        name=format_name,
+        mime_type=mime_type,
+        extensions=format_extensions,
     )
+
+
+def check_file_extension_matches_format(
+    file_name: str, file_format: FileFormat
+) -> bool:
+    used_extension = os.path.splitext(file_name)[1].lower()
+    return len(file_format.extensions) == 0 or used_extension in file_format.extensions
 
 
 def get_file_format_name(file_path: str) -> str:
@@ -106,12 +111,17 @@ def get_mime_type_extensions(mime_type: str) -> list[str]:
     return mimetypes.guess_all_extensions(mime_type)
 
 
-def set_file_format_xattrs(file: dict,
-                           metadata_key: str,
-                           file_format: FileFormat) -> None:
+def set_file_format_xattrs(
+    file: dict,
+    metadata_key: str,
+    file_format: FileFormat,
+    is_extension_matching_format: bool,
+) -> None:
     file_path = get_file_path(file)
     file_xattrs = xattr.xattr(file_path)
     file_xattrs.set(f"{metadata_key}.format-name", str.encode(file_format.name))
     file_xattrs.set(f"{metadata_key}.mime-type", str.encode(file_format.mime_type))
-    file_xattrs.set(f"{metadata_key}.is-extension-matching-format",
-                    str.encode(str(file_format.is_extension_matching_format)))
+    file_xattrs.set(
+        f"{metadata_key}.is-extension-matching-format",
+        str.encode(str(is_extension_matching_format)),
+    )
