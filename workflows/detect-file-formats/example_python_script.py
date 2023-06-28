341397c6f3d99dc07e3b19ceecfce9b433a15626
rwidzisz <rafal.widziszewski@gmail.com>
2023-06-28 19:05:52 +0200

    VFS-10830 Refactored example files

--- workflows/detect-file-formats/example_python_script.py
+++ workflows/detect-file-formats/example_python_script.py
@@ -4,54 +4,54 @@
 # n is size of heap
 
 
-def heapify(arr, N, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1  # left = 2*i + 1
-    r = 2 * i + 2  # right = 2*i + 2
+def heapify(argument, number, iteration):
+    largest = iteration  # Initialize largest as root
+    l = 2 * iteration + 1  # left = 2*i + 1
+    r = 2 * iteration + 2  # right = 2*i + 2
 
     # See if left child of root exists and is
     # greater than root
-    if l < N and arr[largest] < arr[l]:
+    if l < number and argument[largest] < argument[l]:
         largest = l
 
     # See if right child of root exists and is
     # greater than root
-    if r < N and arr[largest] < arr[r]:
+    if r < number and argument[largest] < argument[r]:
         largest = r
 
     # Change root, if needed
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+    if largest != iteration:
+        argument[iteration], argument[largest] = argument[largest], argument[iteration]  # swap
 
         # Heapify the root.
-        heapify(arr, N, largest)
+        heapify(argument, number, largest)
 
 
 # The main function to sort an array of given size
 
 
-def heapSort(arr):
-    N = len(arr)
+def heapSort(argument_):
+    number_ = len(argument_)
 
     # Build a maxheap.
-    for i in range(N // 2 - 1, -1, -1):
-        heapify(arr, N, i)
+    for iteration in range(number_ // 2 - 1, -1, -1):
+        heapify(argument_, number_, iteration)
 
     # One by one extract elements
-    for i in range(N - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
-        heapify(arr, i, 0)
+    for iteration in range(number_ - 1, 0, -1):
+        argument_[iteration], argument_[0] = argument_[0], argument_[iteration]  # swap
+        heapify(argument_, iteration, 0)
 
 
 # Driver's code
-if __name__ == '__main__':
-    arr = [12, 11, 13, 5, 6, 7]
+if __name__ == "__main__":
+    argument__ = [12, 11, 13, 5, 6, 7]
 
     # Function call
-    heapSort(arr)
-    N = len(arr)
+    heapSort(argument__)
+    number__ = len(argument__)
 
     print("Sorted array is")
-    for i in range(N):
-        print("%d" % arr[i], end=" ")
+    for i in range(number__):
+        print(f"{i}" % argument__[i], end=" ")
 # This code is contributed by Mohit Kumra
