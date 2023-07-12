9dbe6d028ac1ac18b93b9734d42bcddee9e79802
rwidzisz <rafal.widziszewski@gmail.com>
2023-07-12 10:37:35 +0200

    VFS-10830 Fixed tasks in PR

--- workflows/detect-file-formats/example_python_script.py
+++ workflows/detect-file-formats/example_python_script.py
@@ -1,60 +1,7 @@
-# Python program for implementation of heap Sort
+from zipfile import ZipFile
 
-# To heapify subtree rooted at index i.
-# n is size of heap
+file_name = "example_zip.zip"
 
-
-def heapify(argument, number, iteration):
-    largest = iteration  # Initialize largest as root
-    l = 2 * iteration + 1  # left = 2*i + 1
-    r = 2 * iteration + 2  # right = 2*i + 2
-
-    # See if left child of root exists and is
-    # greater than root
-    if l < number and argument[largest] < argument[l]:
-        largest = l
-
-    # See if right child of root exists and is
-    # greater than root
-    if r < number and argument[largest] < argument[r]:
-        largest = r
-
-    # Change root, if needed
-    if largest != iteration:
-        argument[iteration], argument[largest] = (
-            argument[largest],
-            argument[iteration],
-        )  # swap
-
-        # Heapify the root.
-        heapify(argument, number, largest)
-
-
-# The main function to sort an array of given size
-
-
-def heapSort(argument_):
-    number_ = len(argument_)
-
-    # Build a maxheap.
-    for iteration in range(number_ // 2 - 1, -1, -1):
-        heapify(argument_, number_, iteration)
-
-    # One by one extract elements
-    for iteration in range(number_ - 1, 0, -1):
-        argument_[iteration], argument_[0] = argument_[0], argument_[iteration]  # swap
-        heapify(argument_, iteration, 0)
-
-
-# Driver's code
-if __name__ == "__main__":
-    argument__ = [12, 11, 13, 5, 6, 7]
-
-    # Function call
-    heapSort(argument__)
-    number__ = len(argument__)
-
-    print("Sorted array is")
-    for i in range(number__):
-        print(f"{i}" % argument__[i], end=" ")
-# This code is contributed by Mohit Kumra
+with ZipFile(file_name, "r") as archive:
+    archive.printdir()
+    archive.extractall()
