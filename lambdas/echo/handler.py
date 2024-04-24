8bb9d065937548c5718eb313292c889ac7d8a30a
wojciech <wojciech05521@gamil.com>
2024-04-24 11:33:36 +0200

    VFS-11784 changed dict zip na comprehension

--- lambdas/echo/handler.py
+++ lambdas/echo/handler.py
@@ -60,7 +60,7 @@ def handle(
     for job_args in job_batch_request["argsBatch"]:
 
         if "wrapResultInArray" in task_config and task_config["wrapResultInArray"]:
-            job_args = dict(zip(job_args.keys(), map(lambda x: [x], job_args.values())))
+            job_args = {arg_name: [arg_value] for arg_name, arg_value in job_args.items()}
 
         if random.random() <= task_config["exceptionProbability"]:
             results.append(AtmException(exception="Random exception"))
